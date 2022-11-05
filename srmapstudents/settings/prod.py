1f5e1b55db50d37172792b7d40b550d0f94e92d5
siddu2355 <siddusirasanambeti@gmail.com>
2022-11-05 12:13:29 +0530

    kjhg

--- srmapstudents/settings/prod.py
+++ srmapstudents/settings/prod.py
@@ -7,17 +7,17 @@ import django_heroku
 DEBUG = False
 
 #Allowed hosts only required when debug is false
-ALLOWED_HOSTS = ['srmapstudents-prod.herokuapp.com']
+ALLOWED_HOSTS = ['afternoon-plains-69747.herokuapp.com']
 
 SECRET_KEY = 'siddu235463'
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'deqjagmmhhf616',
-        'HOST':'ec2-44-209-24-62.compute-1.amazonaws.com',
-        'USER':'yvzlpzgxznojoy',
-        'PASSWORD':'67e9223fcc93300e35d9ec294b2f886a330f79749c5a3fa2a4a742e27cd2e419',
+        'NAME': 'd62kt72v247l77',
+        'HOST':'ec2-35-170-21-76.compute-1.amazonaws.com',
+        'USER':'gndchvlkratekn',
+        'PASSWORD':'aed1a1aeebd73377ae719f581201a3cf79cef24e58a654e21e3923fcc291778e',
         'PORT':'5432',
     }
 }
