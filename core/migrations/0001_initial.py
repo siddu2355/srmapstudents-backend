00f9365c2dd78ddc560e6207c3e755f59cb4c5b7
siddu2355 <siddusirasanambeti@gmail.com>
2022-11-05 17:42:42 +0530

    changed the dependency

--- core/migrations/0001_initial.py
+++ core/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('auth', '0014_alter_user_email_alter_user_first_name_and_more'),
+        ('auth', '0001_initial'),
     ]
 
     operations = [
