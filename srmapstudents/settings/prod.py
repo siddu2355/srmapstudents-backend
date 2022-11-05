9955b6f8491efc4bab9a38476049dbc93054252c
siddu2355 <siddusirasanambeti@gmail.com>
2022-11-05 11:22:49 +0530

    first push

--- srmapstudents/settings/prod.py
+++ srmapstudents/settings/prod.py
@@ -1,6 +1,7 @@
 import os
 from .common import *
 import dj_database_url
+import django_heroku
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -11,6 +12,15 @@ ALLOWED_HOSTS = ['srmapstudents-prod.herokuapp.com']
 SECRET_KEY = os.environ['SECRET_KEY']
 
 DATABASES = {
-    'default':dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'deqjagmmhhf616',
+        'HOST':'ec2-44-209-24-62.compute-1.amazonaws.com',
+        'USER':'yvzlpzgxznojoy',
+        'PASSWORD':'67e9223fcc93300e35d9ec294b2f886a330f79749c5a3fa2a4a742e27cd2e419',
+        'PORT':'5432',
+    }
 }
-import psycopg2
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+django_heroku.settings(locals())
