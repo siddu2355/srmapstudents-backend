d00a656aa09ad32f5d6cb57f01264c2fdfa81c00
siddu2355 <siddusirasanambeti@gmail.com>
2022-11-05 11:27:54 +0530

    sec push

--- srmapstudents/wsgi.py
+++ srmapstudents/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'srmapstudents.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'srmapstudents.settings.prod')
 
 application = get_wsgi_application()
 
