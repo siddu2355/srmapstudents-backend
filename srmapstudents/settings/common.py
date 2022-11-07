22169020567247b6137111d6b398a928d8abd8a6
siddu2355 <siddusirasanambeti@gmail.com>
2022-11-07 09:24:21 +0530

    added cors origin new one

--- srmapstudents/settings/common.py
+++ srmapstudents/settings/common.py
@@ -133,7 +133,7 @@ CORS_ALLOWED_ORIGINS = [
     # "https://sub.example.com",
     "http://localhost:3000",
     "http://127.0.0.1:8000",
-    "https://srmapstudents.herokuapp.com"
+    "https://srmap-students.herokuapp.com"
 ]
 
 AUTH_USER_MODEL = 'core.User'
